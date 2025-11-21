a44154d04d2f52c591c5f70c3c6976b785fb60c2
Puotek <hubertosz.p@gmail.com>
2025-11-21 23:18:08 +0100

    Added basic user model

---
+++ BlazorTry1/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTry1.Models;
+
+public class User {
+    [Required(ErrorMessage = "Id is required")]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Discord User Id is required")]
+    public string DiscordId { get; set; }
+}
