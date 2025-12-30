b73d4c6129753ab5aafc691e51e7458b69f5d993
Puotek <hubertosz.p@gmail.com>
2025-12-30 01:41:47 +0100

    DevService with seed

--- BlazorTry1/Program.cs
+++ BlazorTry1/Program.cs
@@ -1,12 +1,12 @@
 using BlazorTry1;
-using BlazorTry1.Models;
+using BlazorTry1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddDbContext<Context>();
+builder.Services.AddScoped<DevService>();
 
 var app = builder.Build();
 
